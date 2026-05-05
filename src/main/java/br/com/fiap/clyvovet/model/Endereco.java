a4e0e68015156065c04967cc1d8b5308f89a4113
leojp04 <leojp04@gmail.com>
2026-05-04 21:02:14 -0300

    feat: adiciona entidade Tutor com Endereco e enum Sexo

---
+++ src/main/java/br/com/fiap/clyvovet/model/Endereco.java
@@ -0,0 +1,22 @@
+package br.com.fiap.clyvovet.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Endereco {
+    private String logradouro;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String cep;
+}
