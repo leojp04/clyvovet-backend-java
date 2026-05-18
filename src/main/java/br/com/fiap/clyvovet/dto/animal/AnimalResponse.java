2b2108d40771983af54bc63977c0d9230fb30a0b
leop04 <leojp04@gmail.com>
2026-05-18 20:58:45 -0300

    docs: adiciona colecao Insomnia com testes dos endpoints

--- src/main/java/br/com/fiap/clyvovet/dto/animal/AnimalResponse.java
+++ src/main/java/br/com/fiap/clyvovet/dto/animal/AnimalResponse.java
@@ -1,9 +1,8 @@
 package br.com.fiap.clyvovet.dto.animal;
 
-
-
+import br.com.fiap.clyvovet.model.SexoAnimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record AnimalResponse (UUID id, String nome, String raca, String especie, String porte, String cor, String sexo, LocalDate dataNascimento, String observacao, UUID tutorId, String tutorNome) {
+public record AnimalResponse(UUID id, String nome, String raca, String especie, String porte, String cor, SexoAnimal sexo, LocalDate dataNascimento, String observacao, UUID tutorId, String tutorNome) {
 }
